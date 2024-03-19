acfca123d6b54dbca3d4ec422aa960133c7bc4a7
NitheshAcharya <acharnithesh12@gmail.com>
2024-03-19 15:06:03 +0530

    Today added java pattern

---
+++ java/solid_rectangle.java
@@ -0,0 +1,23 @@
+package github.Patterns.java;
+
+/**
+ * solid_rectangle
+ *      *****
+ *      *****
+ *      *****
+ *      *****
+ */
+public class solid_rectangle {
+
+    public static void main(String[] args) {
+        int rows = 4;
+        int colms = 5;
+        // Inner loop
+        for (int i = 0; i < rows; i++) {
+            for(int j = 0;j < colms; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
