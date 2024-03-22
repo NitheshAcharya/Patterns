64a4d972477f582e1d2d70a8359a1e786839bba3
NitheshAcharya <acharnithesh12@gmail.com>
2024-03-22 17:41:25 +0530

    Added new patterns

---
+++ java/zero_one_triangle.java
@@ -0,0 +1,24 @@
+public class zero_one_triangle {
+    
+    /*
+     * 0-1_triangle
+     *      1
+     *      01
+     *      101
+     *      0101
+     *      10101
+     */
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j ++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1);
+                }else {
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
